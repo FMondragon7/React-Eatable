8f9953d1e9bc27235afbb40fcd264392c0c040a4
FMondragon7 <fernandomondragon07@gmail.com>
2023-07-15 21:48:44 -0700

    add: show all name foods

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-function App() {
+import { useEffect, useState } from "react"
+import { getFoods } from "./services/food-service"
+import styled from "@emotion/styled";
 
+function App() {
+  const [foods, setFoods] = useState([])
+
+  useEffect(() => {
+    getFoods().then(setFoods)
+  }, [])
+
+  const Wrapper = styled("div")`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+    
   return (
-    <div>
-      <h1>Eatable Project</h1>
-    </div>
+    <Wrapper>
+      {foods.map((food) => (
+        <div>{food.name}</div>
+      ))}
+    </Wrapper>
   )
 }
 
